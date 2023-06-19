ffbf80266220191ee942e4287b64ca29a3ad1ae0
ww <eshkrab>
2023-06-18 17:13:19 -0700

    fix zmq script

--- zmq_test.py
+++ zmq_test.py
@@ -2,10 +2,7 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.connect("tcp://{SUBSCRIBER_IP}:{SUBSCRIBER_PORT}".format(
-    SUBSCRIBER_IP=<SUBSCRIBER_IP>,
-    SUBSCRIBER_PORT=<SUBSCRIBER_PORT>
-))
-topic = "state"
-message = "on"
+socket.bind("tcp://*:5555")
+topic = "brightness"
+message = "100"
 socket.send_string("%s %s" % (topic, message))

ece1177d0ab3a5404fc776770d31a959dea20688
rina shkrabova <eshkrab@gmail.com>
2023-06-20 08:05:08 -0700

    zmq test qol

--- zmq_sub.py
+++ zmq_sub.py
@@ -1,8 +1,15 @@
 import zmq
+import time
+import sys
 
+if len(sys.argv) < 2:
+    print("Please provide a port number as a command-line argument.")
+    sys.exit(1)
+
+port = sys.argv[1]
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")
+socket.connect(f"tcp://localhost:{port}")
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
 print("I am ready to receive")
@@ -11,5 +18,16 @@ topic = b'brightness\0'
 message = "100"
 
 while True:
-    print(socket.recv_string())
+    received_message = socket.recv_string()
+    print(received_message)
+
+    # Modulate the value based on time
+    current_time = time.time()
+    value = int(current_time % 60)  # Modulate between 0 and 59
+    message = str(value)
+
+    # Perform any necessary processing or actions based on the received message
+    # ...
+
+    time.sleep(0.1)
 
--- zmq_test.py
+++ zmq_test.py
@@ -1,12 +1,18 @@
 import zmq
 import time
+import sys
+
+port = sys.argv[1] if len(sys.argv) > 1 else "5555"
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:5555")
+socket.bind(f"tcp://*:{port}")
+
 topic = "brightness"
 message = "100"
+
 while True:
-    socket.send_string(  topic + message )
-    print("%s %s" % (topic, message))
-    time.sleep(1)
+    socket.send_string(topic + message)
+    print(f"{topic} {message}")
+    time.sleep(0.1)
+
